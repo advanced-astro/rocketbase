f97d8e3610e90be995b47b803200291ef4e9e41c
Marc RDWZ <redwerkz@gmail.com>
2023-08-30 20:13:30 +0200

    🎨  Some improvements and minor fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config'
-
-import sitemap from 'astro-sitemap'
-import compress from 'astro-compress'
-import compressor from 'astro-compressor'
-import robotsTxt from 'astro-robots-txt'
+import { SITE } from './src/data/config';
+import { defineConfig } from 'astro/config';
+import sitemap from 'astro-sitemap';
+import compress from 'astro-compress';
+import compressor from 'astro-compressor';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap(), compress(), compressor(), robotsTxt()],
-  site: 'https://base.advanced-astro.dev'
-})
+  site: SITE.url
+});
--- src/data/config.ts
+++ src/data/config.ts
@@ -1,4 +1,11 @@
+interface SITE {
+  title: string
+  description: string
+  url: string
+}
+
 export const SITE = {
   title: 'Advanced Astro',
-  description: 'Learn how Astro works and explore the official API docs.'
+  description: 'Learn how Astro works and explore the official API docs.',
+  url: 'https://advanced-astro.dev'
 }
